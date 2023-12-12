2492de93e686ca3be259c5f24eb31c20f53f9e0b
Georgii <mga@nn.ru>
2023-12-12 21:45:06 +0300

    sec

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -3,6 +3,6 @@ import styles from '../page.module.css'
 
 export default function Footer() {
   return (
-    <div>footer</div>
+    <div className={styles.footer}>footer</div>
   )
 }
--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -5,7 +5,13 @@ import styles from '../page.module.css'
 export default function Header() {
   return (
     <header>
-        <div><span className={styles.logo}>House Staff</span></div>
+        <div>
+          <span className={styles.logo}>House Staff</span></div>
+          <ul className={styles.nav}>
+            <li>Contact</li>
+            <li>About us</li>
+            <li>Room</li>
+          </ul>
         <div className={styles.presentation}></div>
     </header>
   )
